0b5f44c971d02b8258a5d1b06bcd7228ef9639d6
mathieuboissy <mathieuboissy97@gmail.com>
2019-11-08 13:44:03 -0500

    modified object key

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -18,5 +18,3 @@ with zipfile.ZipFile(portfolio_zip) as myzip:
         portfolio_bucket.upload_fileobj(obj, nm,
          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
-return 'Hello from Lambda'
